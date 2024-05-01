b70a8bf9382c8b8d1cd55ec90b7661dd7dec35b0
tsuyoshi.kumamoto <tsuyoshi.kumamoto@play.jp>
2024-05-01 18:25:31 +0900

    metro.config fix

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,32 @@
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts }
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("./transformer.js")
-    },
-    resolver: {
-      sourceExts: [...sourceExts, "scss", "sass"]
-    }
-  };
-})();
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  }
+};
+
+const {
+  resolver: { sourceExts },
+} = getDefaultConfig(__dirname);
+const sassConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-sass-transformer'),
+  },
+  resolver: {
+    sourceExts: [...sourceExts, 'scss', 'sass'],
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config, sassConfig);
