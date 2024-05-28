bbe38376699fe42dfdcce49a2e7a18cb4de29f1f
tsuyoshi.kumamoto <tsuyoshi.kumamoto@play.jp>
2024-05-28 21:05:44 +0900

    scss記法

--- components/UsingScssStyling.tsx
+++ components/UsingScssStyling.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Text } from  'react-native';
+import { Text, View } from  'react-native';
+// @ts-ignore
+import styles from '../style/components/componentStyle.scss';
 
 /**
  * Scssとreact-native-sass-transformerを利用した手法で記述
@@ -7,14 +9,23 @@ import { Text } from  'react-native';
 
 export default () => {
   return (
-    <React.Fragment>
-      <Text>H1 Text Title Sample</Text>
-      <Text>H2 Text Title Sample</Text>
-      <Text>p Text Paragraph Sample</Text>
-      <Text>Text Style Sample 1</Text>
-      <Text>Text Style Sample 2</Text>
-      <Text>Text Style Sample 3</Text>
-      <Text>Text Style Sample 4</Text>
-    </React.Fragment>
+    <View style={[styles.container, styles.borderBlack, styles.spacing]}>
+      <Text style={[
+        styles.textCommon,
+        styles.spacing,
+        {
+          fontSize: 20
+        }
+      ]}>
+        通常のReact Nativeスタイリング
+      </Text>
+      <Text style={[styles.textCommon, styles.textH1]}>H1 Text Title Sample</Text>
+      <Text style={[styles.textCommon, styles.textH2]}>H2 Text Title Sample</Text>
+      <Text style={[styles.textCommon, styles.textParagraph, styles.textUnderline]}>p Text Paragraph Sample</Text>
+      <Text style={[styles.textCommon, styles.textH3, styles.textRed]}>Text Style Sample 1</Text>
+      <Text style={styles.textCommon}>Text Style Sample 2</Text>
+      <Text style={[styles.textCommon, styles.textUnderline]}>Text Style Sample 3</Text>
+      <Text style={[styles.textCommon, styles.textBlue]}>Text Style Sample 4</Text>
+    </View>
   );
 }
